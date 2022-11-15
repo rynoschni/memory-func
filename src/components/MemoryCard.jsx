667b6973d497c69d0e7704174f5e4ef303daf762
Ryan Schniederjan <ryno@schnietke.com>
2022-11-14 22:04:19 -0600

    Updated Memory Card DIVs

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -3,10 +3,19 @@ import './MemoryCard.css'
 
 function MemoryCard() {
   return (
-    <div className='MemoryCard'>
-      <img src='https://www.digitalcrafts.com/media/Default/assets/logos/dc-logo.svg' className='logo' alt='DigitalCrafts Logo'/>
+    <div className="MemoryCard">
+      <div className="MemoryCardInner">
+        <div className="MemoryCardBack">
+          <img
+            src="https://www.digitalcrafts.com/media/Default/assets/logos/dc-logo.svg"
+            className="logo"
+            alt="DigitalCrafts Logo"
+          />
+        </div>
+        <div className="MemoryCardFront">∆</div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default MemoryCard
