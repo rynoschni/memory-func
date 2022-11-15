65de88b785be2e908437ac38fd49f58e09ade58e
Ryan Schniederjan <ryno@schnietke.com>
2022-11-15 00:33:24 -0600

    Added Flipping logic

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { React, useState } from 'react'
 import './MemoryCard.css'
 
 function MemoryCard() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  function _clickHandler(){
+    if (!isFlipped) {
+      setIsFlipped(true);
+    } else {
+      setIsFlipped(false)
+    }
+    
+  }
   return (
-    <div className="MemoryCard">
-      <div className="MemoryCardInner">
+    <div className="MemoryCard" onClick={_clickHandler}>
+      <div className={isFlipped ? 'MemoryCardInner flipped' : 'MemoryCardInner'}>
         <div className="MemoryCardBack">
           <img
             src="https://www.digitalcrafts.com/media/Default/assets/logos/dc-logo.svg"
