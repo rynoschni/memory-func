0dfc4c1816bb30f85f41a254c0fdd998ae40dbd4
Ryan Schniederjan <ryno@schnietke.com>
2022-11-14 20:50:56 -0600

    End of Part One

---
+++ src/components/MemoryCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MemoryCard.css'
+
+function MemoryCard() {
+  return (
+    <div className='MemoryCard'>
+      <img src='https://www.digitalcrafts.com/media/Default/assets/logos/dc-logo.svg' className='logo' alt='DigitalCrafts Logo'/>
+    </div>
+  )
+}
+
+export default MemoryCard
